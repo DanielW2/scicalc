90630b335031fd3450b987fd99abc6cb5f2870ef
Daniel Williams <daniel@ai-asylum.com>
2014-11-20 00:01:13 -0800

    resize to window
    The app will now correctly fill window though further formatting is
    needed.

--- public/js/scicalc.js
+++ public/js/scicalc.js
@@ -5,7 +5,20 @@ function appController($scope, $http) {
 	$scope.statements = {};
 	
 	$scope.testValue = 'Success!';
-
+/*
+	viewportwidth = 0;
+	viewportheight = 0;
+	  
+	 // the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
+	  
+	 if (typeof window.innerWidth != 'undefined')
+	 {
+		  viewportwidth = window.innerWidth;
+		  viewportheight = window.innerHeight;
+	 }
+	
+	document.body.style.height = viewportheight-60;*/
+	
 	$scope.appendInput = function(input) {
 		$scope.data.textdata=$scope.data.textdata+input;
 	};
@@ -59,4 +72,12 @@ function appController($scope, $http) {
 			$scope.data.textdata = response;
 		});
 	};
+}
+
+window.onload = function(event) {
+	document.body.style.height = window.innerHeight - 60;
+}
+
+window.onresize = function(event) {
+	document.body.style.height = window.innerHeight - 60;
 }
